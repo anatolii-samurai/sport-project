58622d683c5eaeef4171e124d52b5ab760f74047
anatolii-samurai <goddevil22@yandex.ru>
2022-01-26 06:55:36 +0400

    Feed

--- src/js/script.js
+++ src/js/script.js
@@ -20,6 +20,23 @@ $(document).ready(function(){
       // settings: "unslick"
       // instead of a settings object
     ]
-  });
+  });    
+      $('ul.catalogue__tabs').on('click', 'li:not(.catalogue__tab_active)', function() {
+        $(this)
+          .addClass('catalogue__tab_active').siblings().removeClass('catalogue__tab_active')
+          .closest('div.container').find('div.catalogue__content').removeClass('catalogue__content_active').eq($(this).index()).addClass('catalogue__content_active');
+     });
+     function toggleSlide(item) {
+      $(item).each(function(i) {
+          $(this).on('click', function(e) {
+              e.preventDefault();
+              $('.catalogue-item__content').eq(i).toggleClass('catalogue-item__content_active');
+              $('.catalogue-item__list').eq(i).toggleClass('catalogue-item__list_active');
+          })
+      });
+  };
+
+  toggleSlide('.catalogue-item__link');
+  toggleSlide('.catalogue-item__back');
 });
 			
